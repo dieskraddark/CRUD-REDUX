c245ff1ccfd3d50f1b733db8b317df38f233f76e
dieskraddark <kezibigyat1@gmail.com>
2023-11-09 11:55:44 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/actions/Actions.js
@@ -0,0 +1,13 @@
+export const addItems=()=>({
+    type:'ADD_ITEMS'
+   
+
+})
+
+export const updateItems=()=>{
+    type:'UPDATE_ITEMS'
+}
+
+export const deleteItems=()=>{
+    type:'DELETE_ITEMS'
+}
